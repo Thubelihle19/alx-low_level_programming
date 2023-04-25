adbe444eb384eaca3a0a30184c9c115a7532172d
Thubelihle Muriyango <thubelihlendlovu590@gmail.com>
2023-04-25 11:31:39 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,30 @@
-##
+#include "main.h"
+
+/**
+  *print_sign -determines if the input number
+  *greater, equal or less than zero.
+  *
+  *@n: The input number as an intger.
+  *
+  *Return: 1 is greater than than zero. 0 is zero.
+  *-1 is less than zero.
+  */
+int print_signs(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,2 +1,20 @@
 #include "main.h"
-
+/**
+  *_abs - computers the absolute value
+  *of an integer.
+  *
+  *@i;input number as an integer.
+  *
+  *return:absolute value
+  */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
